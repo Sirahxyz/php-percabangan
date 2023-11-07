ff8b54951173911afa9c2224f66b42526997cbd5
Sirahxyz <harisafifudin123@gmail.com>
2023-11-07 17:25:27 +0700

    Commit pertama

---
+++ percabangan.php
@@ -0,0 +1,13 @@
+<?php
+    $nilai = 75;
+
+    if ($nilai >= 80) {
+        echo "Nilai Anda A";
+    } elseif ($nilai >= 70) {
+        echo "Nilai Anda B";
+    } elseif ($nilai >= 60) {
+        echo "Nilai Anda C";
+    } else {
+        echo "Nilai Anda D";
+    }
+?>
